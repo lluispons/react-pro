0a85238212bd681825c40142eeaf7ee6f45a6d54
Lluís Pons <lluis.ponshernandez@plexus.es>
2022-10-26 12:39:10 +0200

    COmponent compund ready

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+import { useProduct } from "../hooks/useProduct";
+
+import {
+  ProductContextProps,
+  ProductcardProps,
+} from "../interfaces/interfaces";
+
+import styles from "../styles/styles.module.css";
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export const ProductCard = ({ product, children }: ProductcardProps) => {
+  const { counter, increaseBy } = useProduct();
+
+  return (
+    <Provider value={{ counter, increaseBy, product }}>
+      <div className={styles.productCard}>{children}</div>
+    </Provider>
+  );
+};
---
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -0,0 +1,25 @@
+import { ReactElement } from "react";
+
+export interface ProductcardProps {
+  children?: ReactElement | ReactElement[];
+  product: Product;
+}
+
+export interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
+
+export interface ProductContextProps {
+  counter: number;
+  increaseBy: (value: number) => void;
+  product: Product;
+}
+
+export interface ProductCardHOCProps {
+  ({ product, children }: ProductcardProps): JSX.Element;
+  Title: ({ title }: { title?: string | undefined }) => JSX.Element;
+  Image: ({ img }: { img?: string | undefined }) => JSX.Element;
+  Buttons: () => JSX.Element;
+}
