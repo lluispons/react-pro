d7e7da1fadabb780ba5218398d14f80ce57c8712
Lluís Pons <luisponshernandez@gmail.com>
2022-10-28 13:47:47 +0200

    control props alternativo

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/interfaces";
 
 interface UseProductArgs {
@@ -14,12 +14,7 @@ export const useProduct = ({
 }: UseProductArgs) => {
   const [counter, setCounter] = useState(value);
 
-  const isControlled = useRef(!!onChange);
-
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({ count: value, product });
-    }
     const newValue = Math.max(counter + value, 0);
 
     setCounter(newValue);
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { onChangeArgs, ProductInCart } from "../interfaces/interfaces";
+import { onChangeArgs, Product, ProductInCart } from "../interfaces/interfaces";
 
 export const useShoppingCart = () => {
   const [shoppingCart, setShoppingCart] = useState<{
@@ -7,29 +7,21 @@ export const useShoppingCart = () => {
   }>({});
 
   const onProductCountChange = ({ count, product }: onChangeArgs) => {
-    // console.log({ count });
-
     setShoppingCart((oldCart) => {
-      const productInCart: ProductInCart = oldCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldCart,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        // * Option 1: Destructuring (more readable but slower and adds unnecessary variable)
+        const { [product.id]: toDelete, ...rest } = oldCart;
+        return rest;
+        // * Option 2: Delete (less readable but a lot faster)
+        // const newCart = {...oldCart};
+        // delete newCart[product.id];
+        // return newCart;
       }
-      // * Option 1: Destructuring (more readable but slower and adds unnecessary variable)
-      const { [product.id]: toDelete, ...rest } = oldCart;
-      return { ...rest };
 
-      // * Option 2: Delete (less readable but a lot faster)
-      // const newCart = {...oldCart};
-      // delete newCart[product.id];
-      // return newCart;
+      return {
+        ...oldCart,
+        [product.id]: { ...product, count },
+      };
     });
   };
 
